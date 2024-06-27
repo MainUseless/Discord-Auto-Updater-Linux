99e76146cad2a418b84622ba90980685cbf02a1d
Mostafa-Hesham <86481662+MainUseless@users.noreply.github.com>
2024-06-27 15:04:52 +0300

    comments

--- discord-updater.sh
+++ discord-updater.sh
@@ -7,3 +7,8 @@ else
     sed -i 's/"[0-9.]*"/"'"$newVersion"'"/' /opt/discord/resources/build_info.json
     /usr/bin/discord
 fi
+
+# change the permission of the file and the directory enable read and execute 
+# /opt/discord/resources & /opt/discord/resources/build_info.json
+# can change the execute command in /usr/bin/discord to the path of this executable to run the script
+# any time you start discord
